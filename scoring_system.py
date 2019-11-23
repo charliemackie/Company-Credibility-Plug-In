e7db3c18b4d2df7d787c64b5dd52546fb345479c
Charlie Mackie <charliemackie@Charlies-MacBook-Pro.local>
2019-11-23 13:10:07 -0500

    Initial commit

---
+++ scoring_system.py
@@ -0,0 +1,11 @@
+# This will be the scoring system for each company
+# Scoring will be based off of 2 factors:
+# 1. Number of transactions where an item was returned (Debits to Customer)
+# 2. Magnitude of the transactions
+
+# Accepting a database (file type?) which will contain every single
+# transaction made by every single customer (Jayson)
+
+# Need to parse these transactions for company names that we are targeting
+# These company names will need to be previously established (Paarth)
+
