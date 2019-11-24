f0e01a88d26f2a81e207a050c02561aba17f8b48
Charlie Lew <37677245+j3lew@users.noreply.github.com>
2019-11-23 23:02:21 -0500

    Upload chrome extension.

---
+++ vault-transaction/background.js
@@ -0,0 +1,20 @@
+// getChildIndex will return the matching element in the object
+function getChildIndex(appState, id) {
+  return appState.values.findIndex(element => element.id == id)
+}
+
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#3aa757'}, function() {
+//     console.log("The color is green.");
+//   });
+// });
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'www.ssense.com', schemes: ['https']}
+    })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
